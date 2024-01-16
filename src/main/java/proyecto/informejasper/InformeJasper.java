270f5a86e1acc2e9661db062073a76d771cfa35b
erick <elio0eri@gmail.com>
2024-01-16 14:21:30 +0000

    Problemas con la ruta relativa

---
+++ src/main/java/proyecto/informejasper/InformeJasper.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto.informejasper;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author erick
+ */
+public class InformeJasper {
+
+    public static void main(String[] args) {
+        GenerarInforme pantalla = new GenerarInforme();
+        pantalla.setVisible(true);
+    }
+}
